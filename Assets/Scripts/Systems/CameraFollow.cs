2223a45971253b10465a6f0e0b5d812146f99b43
Dmitriy Karpus <dmitriy.karpus@gmail.com>
2020-06-08 18:22:41 +0300

    Camera follow by player

---
+++ Assets/Scripts/Systems/CameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class CameraFollow : MonoBehaviour
+{
+    public static CameraFollow instance;
+
+    public Entity playerEntity;
+    public float3 offset;
+
+    private EntityManager manager;
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+
+        manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+    }
+
+    private void LateUpdate()
+    {
+        if (playerEntity == null) { return; }
+        Translation ballPos = manager.GetComponentData<Translation>(playerEntity);
+        transform.position = ballPos.Value + offset;
+    }
+}
--- Assets/Settings/GameManager.cs
+++ Assets/Settings/GameManager.cs
@@ -1,22 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    public GameObject playerPrefab;
     public GameObject cube;
     public Text cubeCountText;
     private int cubeCount = 1;
-    void Start()
+    
+    private Entity playerEntityPrefab;
+    private EntityManager manager;
+    private BlobAssetStore blobAssetStore;
+    
+    private void Awake()
     {
-        cubeCountText.text = cubeCount.ToString();
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+
+        manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        blobAssetStore = new BlobAssetStore();
+        GameObjectConversionSettings settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, blobAssetStore);
+        playerEntityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(playerPrefab, settings);
+    }
+    private void OnDestroy()
+    {
+        blobAssetStore.Dispose();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        cubeCountText.text = cubeCount.ToString();
+        SpawnPlayer();
+    }
+
+    void SpawnPlayer()
+    {
+        Entity newPlayerEntity = manager.Instantiate(playerEntityPrefab);
+
+        Translation playerTrans = new Translation
+        {
+            Value = new float3(0, 0.5f, 0)
+        };
+
+        manager.AddComponentData(newPlayerEntity, playerTrans);
+        CameraFollow.instance.playerEntity = newPlayerEntity;
     }
 
     public void SpawnTenCubes()
