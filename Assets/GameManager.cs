bb0bdbe76867c54cbdf9634925cdb692222704da
oleg.morozov <morozov.oleg7@gmail.com>
2020-06-08 08:22:46 +0300

    Initial commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject cube;
+    public Text cubeCountText;
+    private int cubeCount = 1;
+    void Start()
+    {
+        cubeCountText.text = cubeCount.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnTenCubes()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(cube, new Vector3(i * 3 - 15, 15, 11), Quaternion.identity, transform);
+        }
+
+        cubeCount += 30;
+        cubeCountText.text = cubeCount.ToString();
+    }
+}
